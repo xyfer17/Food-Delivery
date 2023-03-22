7d39acf6795bd357e964892de122aae519dcb096
xyfer17 <nav3044999@gmail.com>
2023-03-23 00:14:28 +0530

    add register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
+import Helmet from "../components/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
 
 const Register = () => {
-  return <div>Register</div>;
+  const signupNameRef = useRef();
+  const signupEmailRef = useRef();
+  const signupPasswordRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Helmet title="SignUp">
+      <CommonSection title="SignUp" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12" className="m-auto text-center">
+              <form className="form mb-5" onSubmit={submitHandler}>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Full Name"
+                    ref={signupNameRef}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    ref={signupEmailRef}
+                  />
+                </div>
+                <div className="form__group">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    ref={signupPasswordRef}
+                  />
+                </div>
+                <button type="submit" className="addToCart__btn">
+                  Sign Up
+                </button>
+              </form>
+              <Link to="/login">Already have an account? Login</Link>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Register;
